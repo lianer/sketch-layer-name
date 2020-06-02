5b28312689d0d94924dfc743ad56b1dfe48088fe
楼云龙 <louyl@wedoctor.com>
2020-06-02 18:40:41 +0800

    feat: 使用 onDocumentChanged API 代替 ArtboardChanged API

--- src/rename-to-style-name.js
+++ src/rename-to-style-name.js
@@ -1,8 +1,9 @@
 import sketch from 'sketch';
-import rename from './rename';
-// documentation: https://developer.sketchapp.com/reference/api/
+import { log, isLayer, getAllSubLayers, checkLayerName } from './utils';
 
 /*
+documentation: https://developer.sketchapp.com/reference/api/
+
 1. 可以安装 Sketch 官方提供的 devTool 以方便调试，可以通过 devTool 查看用户行为触发了哪些事件
 2. Sketch 官方的开发文档比较简陋，基本上无法只通过阅读文档开发出一个成品插件
 3. 该插件参考了：https://github.com/tbrasington/rename-layers-based-on-textstyle
@@ -11,89 +12,26 @@ import rename from './rename';
 6. document, page, artBoard, layer, sharedStyle 等都有一些自带的属性（比如：name、class），但获取它们并不能直接通过 layer.name 的方式来获取，而是要通过 layer.name() 的方式来获取（可以把 layer.name() 理解为一次 jsBridge 通信）
 7. layer 是有嵌套关系的，是一个树结构，因此如果要获取一个 artBoard 中所有的 layer，应当递归遍历它们
 8. layer.class() === MSLayerGroup 来判断 layer 是一个组还是一个图层
+9. onDocumentChanged 是 Sketch 59 之后提供的 API，这一年来有过许多变更，因此也不能保证该插件可以兼容后期的 Sketch 版本
+10. onDocumentChanged 事件返回的数据结构：context > actionContext(changes) > object, id, property...
 */
 
-const isFunction = function (fn) {
-  return typeof fn === 'function';
-};
-
-// 递归查找所有子图层（包含入参 layer 本身）
-const getAllSubLayers = function (layer) {
-  const layers = [];
-  const recursionFindLayers = function (layer) {
-    layers.push(layer);
-    // 判断是否有子图层
-    // 1. 可根据类判断（但这样判断容易缺少类型，未来 Sketch 如果有新增加类型，则兼容性会不太好）
-    //    layer.class() === MSLayerGroup || layer.class() === MSArtboardGroup || layer.class() === MSShapeGroup
-    // 2. 也可根据是否有 layers 函数判断
-    //    typeof layer.layers === 'function'
-    if (isFunction(layer.layers)) {
-      layer.layers().forEach(recursionFindLayers);
-    }
-  };
-  recursionFindLayers(layer);
-  return layers;
-};
-
-// 检查所有图层的共享样式和名称并修改图层名称
-const checkLayersName = function (layers) {
-  layers.forEach((layer, i) => {
-    const oldName = layer.name();
-    if (isFunction(layer.sharedStyle)) {
-      const sharedStyle = layer.sharedStyle();
-      if (sharedStyle) {
-        const styleName = sharedStyle.name();
-        const newName = rename(oldName, styleName);
-        if (newName !== oldName) {
-          layer.name = newName;
-        }
-      }
-    }
-  });
-};
-
 // 监听文档变化，Sketch 59 之后有的 API
-// TODO: 这个函数主要用于优化性能，理论上来说提供用户主动触发和 ArtboardChanged 两个途径基本已满足使用，如果视觉那边确定推动摹客了，那再继续完善此处的逻辑
 // Document Changes：https://developer.sketch.com/plugins/document-changes
 export function onDocumentChanged(context) {
+  // console.time('onDocumentChanged');
+  // log('------------ onDocumentChanged begin ------------');
+
   const changes = context.actionContext;
   changes.forEach((change) => {
-    const type = change.type();
-    // 1 - 属性变化
-    if (type === 1) {
-      // const properties = change.propertyName;
-      // console.log(
-      //   change,
-      //   change.propertyName(),
-      //   change.object(),
-      //   change.object().sharedStyle()
-      // );
+    const obj = change.object();
+    if (isLayer(obj)) {
+      checkLayerName(obj);
     }
   });
-}
 
-// 监听 ArtboardChanged 事件，并执行重命名
-export function onArtboardChanged(context) {
-  // 如果是事件触发的函数，则需要读取 context.actionContext
-  context = context.actionContext;
-  const { document, oldArtboard, newArtboard } = context;
-  try {
-    // 经过测试 oldArtboard 不是太准确，在两个 artBoard 之间切换的时候，oldArtboard 的值始终为 null
-    // 因此无法做到只更新 oldArtboard 的图层名称
-    // console.log('oldArtboard:', oldArtboard);
-    // console.log('newArtboard:', newArtboard);
-    const pages = document.pages();
-    const layers = [];
-    pages.forEach((page) => {
-      page.artboards().forEach((artboard) => {
-        layers.push(...getAllSubLayers(artboard));
-      });
-    });
-    checkLayersName(layers);
-  } catch (e) {
-    document.showMessage(e.message);
-    console.error(e);
-  }
+  // log('------------ onDocumentChanged end ------------');
+  // console.timeEnd('onDocumentChanged');
 }
 
 // 点击菜单 插件 -> sketch-layer-name -> rename-to-style-name 按钮，执行重命名
@@ -107,9 +45,31 @@ export function renameToStyleName(context) {
         layers.push(...getAllSubLayers(artboard));
       });
     });
-    checkLayersName(layers);
+    layers.forEach(checkLayerName);
   } catch (e) {
     document.showMessage(e.message);
     console.error(e);
   }
 }
+
+// 弃用，监听 ArtboardChanged.begin 事件，并执行重命名
+// export function onArtboardChanged(context) {
+//   // 如果是事件触发的函数，则需要读取 context.actionContext
+//   context = context.actionContext;
+//   const { document, oldArtboard, newArtboard } = context;
+//   try {
+//     // 经过测试，在两个 artBoard 之间切换的时候，oldArtboard 的值始终为 null
+//     // 因此无法做到只更新 oldArtboard 的图层名称
+//     const pages = document.pages();
+//     const layers = [];
+//     pages.forEach((page) => {
+//       page.artboards().forEach((artboard) => {
+//         layers.push(...getAllSubLayers(artboard));
+//       });
+//     });
+//     layers.forEach(checkLayerName);
+//   } catch (e) {
+//     document.showMessage(e.message);
+//     console.error(e);
+//   }
+// }
---
+++ src/utils.js
@@ -0,0 +1,68 @@
+export const log = function (...args) {
+  const d = new Date();
+  const f = (v) => (v < 10 ? '0' + v : String(v));
+  const t = `${f(d.getHours())}:${f(d.getMinutes())}:${f(d.getSeconds())}`;
+  console.log(t, ...args);
+};
+
+export const isFunction = function (fn) {
+  return typeof fn === 'function';
+};
+
+export const isLayer = function (obj) {
+  return !!obj && isFunction(obj.layers);
+};
+
+// 递归查找所有子图层（包含入参 layer 本身）
+export const getAllSubLayers = function (layer) {
+  const layers = [];
+  const recursionFindLayers = function (layer) {
+    layers.push(layer);
+    // 判断是否有子图层
+    // 1. 可根据类判断（但这样判断容易缺少类型，未来 Sketch 如果有新增加类型，则兼容性会不太好）
+    //    layer.class() === MSLayerGroup || layer.class() === MSArtboardGroup || layer.class() === MSShapeGroup
+    // 2. 也可根据是否有 layers 函数判断
+    //    typeof layer.layers === 'function'
+    if (isFunction(layer.layers)) {
+      layer.layers().forEach(recursionFindLayers);
+    }
+  };
+  recursionFindLayers(layer);
+  return layers;
+};
+
+// 文本重命名，e.g: 请选择 => # Reg/14/neu7 # 请选择
+export const rename = function (name, styleName) {
+  const leftLimit = '# ';
+  const rightLimit = ' # ';
+  const leftIndex = name.indexOf(leftLimit);
+  const rightIndex = name.indexOf(rightLimit, leftIndex + 1);
+
+  if (leftIndex > -1 && rightIndex > -1) {
+    return (
+      leftLimit +
+      styleName +
+      rightLimit +
+      name.slice(rightIndex + rightLimit.length)
+    );
+  }
+  return leftLimit + styleName + rightLimit + name;
+};
+// 文本1  =>  // # core1 # 文本1
+// console.log(rename('文本1', 'core1'));
+// console.log(rename('//neu9//按钮2', 'core1'));
+// console.log(rename('//core1//图片3', 'core1'));
+
+// 检查所有图层的共享样式和名称并修改图层名称
+export const checkLayerName = function (layer) {
+  if (isFunction(layer.sharedStyle)) {
+    const sharedStyle = layer.sharedStyle();
+    if (sharedStyle) {
+      const oldLayerName = layer.name();
+      const newLayerName = rename(oldLayerName, sharedStyle.name());
+      if (newLayerName !== oldLayerName) {
+        layer.name = newLayerName;
+      }
+    }
+  }
+};
